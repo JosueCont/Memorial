8832c86d8acf85045129cffeb65b1193483f8925
Josue Contreras <josue.contreras@hiumanlab.com>
2025-03-19 19:01:42 -0600

    Initial Commit

---
+++ Memoria/MemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaApp.swift
+//  Memoria
+//
+//  Created by Josue Contreras on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
